108299755dbcb0667ef688d66d25e35f6cd7a947
code-Kartik-Rathore <143636920+code-Kartik-Rathore@users.noreply.github.com>
2024-05-28 17:50:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,68 +1,32 @@
-
-import './App.css';
-import Sign from './SIGNUP/Sign';
+import "./App.css";
+import Sign from "./SIGNUP/Sign";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./LOGIN/Login";
+import Main from "./mainPage/Main2";
+import Main2 from "./mainPage/Main2";
 // import React, { useState } from "react";
 
-
 function App() {
-  //   const [currentForm, setCurrentForm] = useState('Login');
-
-  // const toggleForm = (formName) => {
-  //   setCurrentForm(formName);
-  // }
+ 
   const route = createBrowserRouter([
+   
     {
       path: "/",
-      element: <Sign/>,
+      element: <Sign />,
     },
     {
       path: "/login",
       element: <Login />,
     },
-
   ]);
-  
+
   return (
     <div className="App">
       <RouterProvider router={route}></RouterProvider>
-      {/* currentForm === "Login" ? <Login onFormSwitch={toggleForm} /> :{" "}
-      <Sign onFormSwitch={toggleForm} /> */}
+     
     </div>
   );
 }
 
 export default App;
-
-
-// // import logo from './logo.svg';
-// import './App.css';
-// // import { Login } from "./myComponents/Login";
-// import LoginForm  from "./myComponents/LoginForm";
-// import { Register } from "./myComponents/signin";
-
-// function App() {
-//   const [currentForm, setCurrentForm] = useState('login');
-
-//   const toggleForm = (formName) => {
-//     setCurrentForm(formName);
-//   }
-
-//   return (
-//     <div className="App">
-//       {
-//         currentForm === "login" ? <LoginForm onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-//       }
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
 
